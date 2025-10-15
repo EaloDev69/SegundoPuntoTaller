1dbc490984a7d287caca7bffd7937001073f9c2a
EaloDev69 <sansastico20@gmail.com>
2025-10-15 12:53:59 -0500

    active el git

---
+++ src/main/java/com/mycompany/segundopuntotaller_miguelangelealovalle/SegundoPuntoTaller_miguelangelealovalle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.segundopuntotaller_miguelangelealovalle;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class SegundoPuntoTaller_miguelangelealovalle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
